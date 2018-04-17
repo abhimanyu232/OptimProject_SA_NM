b58a911f44a6be74c2d7cc8d392793b4730b61d2
Abhimanyu <bhadaura@student.ethz.ch>
2018-04-18 00:46:40 +0200

    simpler code

--- driver.cpp
+++ driver.cpp
@@ -3,36 +3,51 @@
 
 int main(){
 
-/*
-// rough idea : implement with templates
-template <typename Derived>
-void (*foo)(const MatrixBase<Derived>&)
+int dim = 2;
+cout << "enter search space dimension ( 2 <= Dim <= DIM_MAX ) \n" << endl;
+while (!(cin >> dim) || dim < 2 || dim > DIM_MAX){
+  cout << " value out of range ( 2 <= Dimension <= " << DIM_MAX << " )" << '\n';
+}
+
+double (*foo)(int  , const VectorXd& );
+
 cout << "Please choose the test function ; Enter 1 , 2 , 3 OR 4\n "
 "\tCase 1: Rosenbrock 2D \n\tCase 2:Rosenbrock Multidimensional \n\t"
 "Case 3: Sphere Multidimensional \n\t Case 4: Eggholder 2D" << endl;
 int choice;
 while (!(cin>>choice) || choice > 4 || choice < 1){
-  cin.clear()
-  cin.ignore(std::numeric_limits<std::streamsize>::max(),"\n");
-  cout<<"please enter integers only"<<endl;
+  cin.clear();
+  cin.ignore(1000,'\n');
+  //cin.ignore(std::numeric_limits<std::streamsize>::max(),"\n");
+  cout<<"please enter integers 1 2 3 or 4 only"<<endl;
   cin >> choice;
 }
 switch (choice) {
-  case 1: foo = rosenbrock_2d;  break;
-  case 2: foo = rosenbrock_Nd; break;
-  case 3: foo = sphere_Nd; break;
-  case 4: foo = egghol; break;
+  case 1:
+    foo = rosenbrock_2d;
+    break;
+
+  case 2:
+    foo = rosenbrock_Nd;
+    break;
+
+  case 3:
+    foo = sphere_Nd;
+    break;
+
+  case 4:
+    foo = egghol;
+    break;
 }
 
 // rough idea : implement with templates
-*/
 
 
-Vector2d v1;
-v1 << 1,1;
-//v1 << Vector2d::Random();
-cout <<"x :" << v1(0) << "y :" << v1(1)<< endl;
-double fit = rosenbrock_2d(1,100,v1);
+VectorXd X(dim);
+X = VectorXd::Random(dim);
+cout <<"X: \n" << X << endl;
+
+double fit = foo(dim,X);
 cout << "fitness = " << fit << endl;
 
   return 0;
--- optimisation.h
+++ optimisation.h
@@ -1,20 +1,22 @@
 #include<iostream>
 #include<Eigen/Core>
 #include<math.h>
+#include<assert.h>
+
+#define DIM_MAX 10
+
 
 using namespace Eigen;
 using namespace std;
 
-template <typename Derived> // use Vector2d
-double  rosenbrock_2d(double a, double b, const MatrixBase<Derived>& X);
+//template <typename Derived> // use Vector2d
+double rosenbrock_2d(int dim, const VectorXd& X);
 
-template <typename Derived> // use VectorXd
-double rosenbrock_Nd(int dim,double a,double b,const MatrixBase<Derived>& X);
+//template <typename Derived> // use VectorXd
+double rosenbrock_Nd(int dim ,const VectorXd& X);
 
-template <typename Derived>// use VectorNd
-double sphere_Nd(int dim,const MatrixBase<Derived>& X);
+//template <typename Derived>// use VectorNd
+double sphere_Nd(int dim,const VectorXd& X );
 
-template <typename Derived>// use Vector2d
-double egghol(const MatrixBase<Derived>& X);
-
-#include"test_functions.hpp"
+//template <typename Derived>// use Vector2d
+double egghol(int dim, const VectorXd& X);
