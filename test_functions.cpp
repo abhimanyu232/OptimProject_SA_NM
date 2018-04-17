b03e28cbb0d13ecbab30ffefdd4a7e758551ec1f
Abhimanyu <bhadaura@student.ethz.ch>
2018-04-17 03:20:01 +0200

    added more fitness functions

--- test_functions.cpp
+++ test_functions.cpp
@@ -1,4 +1,5 @@
 //#include"test_functions.h"
+
 #include<iostream>
 #include<Eigen/Dense>
 #include<math.h>
@@ -7,12 +8,38 @@ using namespace Eigen;
 using namespace std;
 
 template <typename Derived>
-long rosenbrock_2d(double a, double b, const MatrixBase<Derived>& X  ){
-
+double  rosenbrock_2d(double a, double b, const MatrixBase<Derived>& X  ){
 double r2d =  pow(a - X(0),2) + b*pow(X(1) - X(0),2);
 
   return r2d;
 }
+
+template <typename Derived>
+double rosenbrock_Nd(int dim, double, double, const MatrixBase<Derived>& X){ // use VectorXd
+
+}
+
+template <typename Derived>
+double sphere_3d(int dim,const MatrixBase<Derived>& X){ // VectorNd
+double sph3D = 0;
+for (int i =0; i<=dim ; i++)
+ sph3D = sph3D + pow(X(i),2);
+
+return sph3D;
+}
+
+template <typename Derived>
+double egghol(const MatrixBase<Derived>&){ //use Vector2d
+double eggh;
+
+
+return eggh;
+}
+
+
+
+
+
 
 
 int main(){
