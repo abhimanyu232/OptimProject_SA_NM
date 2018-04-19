0adc42a3e295ca639c53e5cc06fb47a5ae59dd24
Abhimanyu <bhadaura@student.ethz.ch>
2018-04-19 11:54:52 +0200

    updated the simulated annealing files

--- simAnn.cpp
+++ simAnn.cpp
@@ -1,11 +1,9 @@
 
 // simulated annealing //
-
+/*
 VectorXd = Matrix<double, dim, 1>
 RowVectorXd = Matrix<double,1,dim>
-
 RowVectorXd curr, new, best_pt
-
 
 1.  Temperature = 100 ; curr = random() ; best_pt = curr;
 do {
@@ -19,5 +17,41 @@ else if fit(new) > fit(curr) && p(k,Temp) > rand()
           }
 4. Temp = Temp*whatever [cooling scheme]
   }
+5. // REANNEALING??
+*/
 
-// REANNEALING??
+#include "optimisation.h"
+#include "simAnn.h"
+
+int simAnnealing( VectorXd X, double (*fit)() ){
+  double temp;
+  int choice = 0;
+
+  cout << "select cooling scheme:\n"
+  "1: temp = T0*pow(0.95,k)\n "
+  "2: temp = T0/k\n"
+  "3: temp = T0/log(k)\n";
+  while( !(std::cin >> choice;) || (choice < 1) || (choice > 3) ){
+    std::cout << "please enter a valid choice\n "
+    "1: temp = T0*pow(0.95,k)\n "
+    "2: temp = T0/k\n"
+    "3: temp = T0/log(k)\n";
+  }
+
+
+return 0;
+}
+
+
+// k annealing parameter // same as iteration until reannealing //
+bool cooling(const int choice, int k, double *temp){
+  switch (choice) {
+  case 1: *temp = T0*pow(0.95,k); return 1;
+
+  case 2: *temp = T0/k; return 1;
+
+  case 3: *temp = T0/log(k); return 1;
+
+  default: cout << "invalid cooling choice" << '\n'; return 0;
+}
+}
---
+++ simAnn.h
@@ -0,0 +1,6 @@
+##include "optimisation.h"
+
+#define TOL 1e-16
+#define T0  100
+
+double cooling(int choice, double temp);
